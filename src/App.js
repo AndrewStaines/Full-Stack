1c7a6319e4063f48aa48ef0c5a3edc19c047bce7
Andrew William Staines M <93570368+AndrewStaines@users.noreply.github.com>
2023-09-22 15:00:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+	const [abs, setabs] = useState("");
+	const [desc, setdesc] = useState("");
+	const handleOnSubmit = async (e) => {
+		e.preventDefault();
+		let result = await fetch(
+		'http://localhost:2000/register', {
+			method: "post",
+			body: JSON.stringify({ abs, desc }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		result = await result.json();
+		if (result) {
+			alert("Data saved succesfully");
+			setdesc("");
+			setabs("");
+		}
+		
+	}
+	return (
+		<>
+			<div className='acontent'>
+				<h1>Ministry Of Ayush</h1>
+				<div className='content'>
+					<form action="">
+					<div class="mb-3">
+  <label for="formFile" class="form-label">Abstract</label><br/>
+  <input class="form-control" type="file" id="formFile" value={abs} onChange={(e) => setabs(e.target.value)}></input>
+</div>
+						<div class="mb-2">
+  <label for="exampleFormControlTextarea1" class="form-label">Description</label><br/>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={desc} onChange={(e) => setdesc(e.target.value)}></textarea>
+</div>
+						<div className='button'>
+						<button type="button" class="bt" onClick={handleOnSubmit}>Post</button>
+						</div>
+					</form>
+				</div>
+			</div>
+
+		</>
+	);
+}
+
+export default App;
+
